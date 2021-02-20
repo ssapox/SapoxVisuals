d710a20d9d87a4cc75301b1fd139d9d5c7cebf09
ssapox <78848324+ssapox@users.noreply.github.com>
2021-02-20 16:20:48 +0100

    Add files via upload

---
+++ secret.js
@@ -0,0 +1,12 @@
+/*
+function fonctionSecretCode(){
+	let i = window.prompt ("Entrez le code secret");
+	if (i == "NSI"){
+		window.alert("Bienvenue !");
+	}
+	else{
+		fonctionSecretCode();
+	}
+}
+window.onload = fonctionSecretCode();
+*/
